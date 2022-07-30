58625f7eeeb74a96509e273fed991a48a7adbd64
vidit-singhal <viditsinghal2829@gmail.com>
2022-07-30 15:08:19 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -20,6 +20,11 @@ function App() {
           <h1>DIGITAL CLOCK</h1>
         </div>
         <Clock />
+        <span className="colorChangeText1">
+          Click anywhere to change the color
+        </span><span className="colorChangeText2">
+          Move your mouse to change the color
+        </span>
       </div>
     </>
   );
@@ -29,6 +34,8 @@ window.addEventListener('mousemove', function (e) {
   let x = (e.x / 6) * 2;
   let y = (e.y / 6) * 2;
   document.getElementsByClassName("header-text")[0].style.color = "rgb(" + x + ", 120, " + y + ")"
+  document.getElementsByClassName("colorChangeText1")[0].style.color = "rgb(" + x + ", 120, " + y + ")"
+  document.getElementsByClassName("colorChangeText2")[0].style.color = "rgb(" + x + ", 120, " + y + ")"
 })
 
 
